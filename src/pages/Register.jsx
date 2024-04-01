3bb2ade22485e4f7c41d9f57a22c32f1e45b55f3
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-01 23:09:49 +0000

    Register method is completed Using Axios

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,72 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
+import axios from "axios";
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://127.0.0.1:8000/api/register", formData)
+      .then((response) => {
+        console.log("Registration successful:", response.data);
+      })
+      .catch((error) => {
+        console.log("Registration error:", error.response.data);
+      });
+  };
   return (
     <>
       <div className="background">
         <div className="shape"></div>
         <div className="shape"></div>
       </div>
-      <form className="h-fit">
+      <form className="h-fit" onSubmit={handleSubmit}>
         <h3 className="font-titles">Get Started Now</h3>
 
-        <label htmlFor="username">Username</label>
-        <input type="text" placeholder="User Name" id="username" />
+        <label htmlFor="name">Username</label>
+        <input
+          type="text"
+          placeholder="User Name"
+          name="name"
+          onChange={handleChange}
+          id="name"
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" placeholder="Email" id="email" />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+          id="email"
+        />
 
         <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Password" id="password" />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleChange}
+          id="password"
+        />
 
-        <button className="login-btn">Register</button>
+        <button className="login-btn" type="submit">
+          Register
+        </button>
 
         <label>
           Already have an account? <br />
-          <a className="text-blue-600" href="#">
+          <a className="text-blue-600" href="/login">
             Login now
           </a>
         </label>
