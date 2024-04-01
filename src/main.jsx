cf41e395eb4e04f95bfc1f95bdfa38531e06cc15
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-01 15:50:12 +0000

    BugFix : Router Bugs ,, Working Good

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import App from './App.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Router>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Router> */}
-    <App />
+  <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 );
