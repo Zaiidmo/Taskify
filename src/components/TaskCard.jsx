0993879bfd193b7ca9691f725e7868666d2a91eb
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-01 16:10:12 +0000

    Create the Task Card component

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,10 @@
+export const TaskCard = () => {
+  return (
+    <div className="bg-transparent border border-gray-700 p-4 font-poppins rounded-lg shadow-lg">
+      <h2 className="text-2xl text-Black font-semibold">Task</h2>
+      <p className="text-Black">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+      </p>
+    </div>
+  );
+};
