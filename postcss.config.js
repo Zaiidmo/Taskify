e44095c0a62d9ce59f04f19f7de735d2278b5848
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-01 15:45:54 +0000

    BugFix : PostCss Vite Bug Fixed

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
+import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import postcssImport from 'postcss-import';
+
 export default {
   plugins: [
+    tailwindcss,
     autoprefixer,
-    postcssImport,
   ],
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx'
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
-import './index.css'
-
-const router = createBrowserRouter() ([
-  { path: '/', element: <App /> },
-  { path: '/login', element: <Login /> },
-  { path: '/register', element: <Register /> }
-]);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from './App.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    {/* <Router>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Router> */}
+    <App />
+  </React.StrictMode>
+);
