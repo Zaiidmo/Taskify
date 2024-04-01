3b8c49a0c101a79aae7226098067f6706bbf34ed
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-01 14:00:06 +0000

    Create Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { NavBar } from "./components/NavBar";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
+import { Footer } from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,8 @@ function App() {
     <>
      <NavBar />
      {/* <Login /> */}
-     <Register />
+     {/* <Register />  */}
+     <Footer />
     </>
   );
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,34 @@
 export const Register = () => {
     return (
       <>
-        
+        <div className="background">
+          <div class="shape"></div>
+          <div class="shape"></div>
+        </div>
+        <form className="h-fit">
+          <h3 className="font-titles">Get Started Now</h3>
+  
+          <label for="username">Username</label>
+          <input type="text" placeholder="User Name" id="username" />
+  
+          <label for="email">Email</label>
+          <input type="email" placeholder="Email" id="email" />
+
+          <label for="password">Password</label>
+          <input type="password" placeholder="Password" id="password" />
+  
+          <button className="login-btn">Register</button>
+  
+          <label>
+            Already have an account? <br />
+            <a
+              className="text-blue-600"
+              href="#"
+            >
+              Login now 
+            </a>
+          </label>
+        </form>
       </>
     );
   };
