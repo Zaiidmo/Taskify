47173718692c97386fc7769e840d48520c0b9aad
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-01 14:44:25 +0000

    Create Pages Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { NavBar } from "./components/NavBar";
-import { Login } from "./components/Login";
-import { Register } from "./components/Register";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 import { Footer } from "./components/Footer";
 import { SideBar } from "./components/SideBar";
 
@@ -11,8 +11,6 @@ function App() {
   return (
     <>
      <NavBar />
-     {/* <Login /> */}
-     {/* <Register />  */}
      <SideBar />
      {/* <Footer /> */}
     </>
